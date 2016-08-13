87e01fba8560dbe0b6b55b03ba60cae696f664ed
Walter Montes <walmon>
2016-08-13 09:21:24 -0500

    Example working

---
+++ DocumentDBHandling/DocumentManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentDBHandling
+{
+    public class DocumentManager
+    {
+        // Cliente de conexion
+        public DocumentClient client;
+        // Nombre en "cache" de la coleccion para poder referenciar
+        public string mainCollection;
+        // Instancia de base de datos
+        Database db;
+        string dbPath;
+        
+        public DocumentManager()
+        {
+            // Los campos EndpointUri y AuthorizationKey salen de los settings de la coleccion en Azure
+            // DatabaseName y CollectionName son los que le pusimos al crearla
+            client = new DocumentClient(new Uri("<EndpointUri>"), "<AuthorizationKey>");
+            db = client.CreateDatabaseQuery().Where(x => x.Id == "<DatabaseName>").AsEnumerable().FirstOrDefault();
+            dbPath = $"dbs/{db.Id}";
+            var collection = client.CreateDocumentCollectionQuery(dbPath)
+                .Where(c => c.Id == "<CollectionName>").AsEnumerable().FirstOrDefault();
+            mainCollection =
+                collection == null ? String.Empty : collection.SelfLink;
+        }
+    }
+}
---
+++ DocumentDBHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentDBHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentManager dm = new DocumentManager();
+            var entity = new Usuario() { Id = "12346665", Name = "Foo Name" };
+
+            // Crear un objeto
+            dm.client.CreateDocumentAsync(dm.mainCollection, entity);
+
+            // Obtener un objeto
+            var savedEntity= dm.client.CreateDocumentQuery(dm.mainCollection)
+                .Where(x => x.Id == "12346665").AsEnumerable().FirstOrDefault();
+
+            entity.Name = "Foo Name Cooler";
+            // Actualizar un objeto
+            dm.client.ReplaceDocumentAsync(savedEntity.SelfLink, savedEntity);
+        }
+    }
+    public class Usuario
+    {
+        public string Name { get; set; }
+        // Debe tener esta propiedad para que la utilice como un Id "predecible" por nosotros
+        public string Id { get; set; }
+    }
+}
